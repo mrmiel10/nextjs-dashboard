97394a610d8535ba115785eef1e7fc776cf9f037
Daryl <borismbakop611@gmail.com>
2024-07-09 01:19:04 +0100

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter,Poppins } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin']});
+export const poppins  = Poppins ({
+    weight: ['400', '700'],
+  style: ['normal', 'italic',],
+  subsets: ['latin'],
+  display: 'swap',
+  })
