88a527be68d24cb918a3bf77c079e1a9c6bada63
Daryl <borismbakop611@gmail.com>
2024-07-11 05:34:51 +0100

    add sonner

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Toaster as Sonner } from "sonner";
+import { CircleX } from "lucide-react";
+import { CircleCheckIcon } from 'lucide-react';
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +11,17 @@ export default function RootLayout({
   return (
     <html lang="en">
        <body className={`${inter.className} antialiased`}>{children}</body>
+       <Sonner
+    
+    closeButton
+richColors
+
+icons={{
+  error: <CircleX className="size-5" />,
+  success: <CircleCheckIcon className="size-5" />,
+}}
+
+/>
     </html>
   );
 }
