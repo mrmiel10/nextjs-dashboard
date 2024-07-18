8d7485edb8f2b16043e94e716df603fe7654ba9a
Daryl <borismbakop611@gmail.com>
2024-07-18 03:33:36 +0100

    add breadcrumb

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,16 +1,7 @@
 import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
-import {
-  Breadcrumb,
-  BreadcrumbEllipsis,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import Link from "next/link"
+
 export default async function InvoiceCreate() {
   // await new Promise((resolve) => setTimeout(resolve, 3000));
   //  // const customers = await fetchCustomers();
@@ -56,29 +47,7 @@ export default async function InvoiceCreate() {
   
   return (
     <main>
-      <Breadcrumb>
-      <BreadcrumbList>
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="/">Home</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbEllipsis />
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbLink asChild>
-            <Link href="/docs/components">Components</Link>
-          </BreadcrumbLink>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
-        </BreadcrumbItem>
-      </BreadcrumbList>
-    </Breadcrumb>
+     
       <Breadcrumbs
         breadcrumbs={[
           { label: 'Invoices', href: '/dashboard/invoices' },
--- app/ui/invoices/breadcrumbs.tsx
+++ app/ui/invoices/breadcrumbs.tsx
@@ -1,37 +1,70 @@
-import { clsx } from 'clsx';
-import Link from 'next/link';
-import { poppins } from '../fonts';
+import { clsx } from "clsx";
+import Link from "next/link";
+import { poppins } from "../fonts";
 
-interface Breadcrumb {
+interface typeBreadcrumb {
   label: string;
   href: string;
   active?: boolean;
 }
+import {
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
 
 export default function Breadcrumbs({
   breadcrumbs,
 }: {
-  breadcrumbs: Breadcrumb[];
+  breadcrumbs: typeBreadcrumb[];
 }) {
   return (
-    <nav aria-label="Breadcrumb" className="mb-6 block">
-      <ol className={clsx(poppins.className, 'flex text-xl md:text-2xl')}>
-        {breadcrumbs.map((breadcrumb, index) => (
-          <li
-            key={breadcrumb.href}
-            aria-current={breadcrumb.active}
-            className={clsx(
-              "mt-2",
-              breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
-            )}
-          >
-            <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
-            {index < breadcrumbs.length - 1 ? (
-              <span className="mx-3 inline-block">/</span>
-            ) : null}
-          </li>
-        ))}
-      </ol>
-    </nav>
+    <Breadcrumb className="mb-6">
+      <BreadcrumbList className="text-xl md:text-2xl">
+        {breadcrumbs.map((breadcrumb, index) =>
+          breadcrumb.active === true ? (
+            <>
+              <BreadcrumbItem className="text-gray-900">
+                <BreadcrumbPage>{breadcrumb.label}</BreadcrumbPage>
+              </BreadcrumbItem>
+              {index < breadcrumbs.length - 1 ? <BreadcrumbSeparator /> : null}
+            </>
+          ) : (
+            <>
+              <BreadcrumbItem>
+                <BreadcrumbLink asChild className="text-gray-500">
+                  <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+                </BreadcrumbLink>
+              </BreadcrumbItem>
+              {index < breadcrumbs.length - 1 ? <BreadcrumbSeparator /> : null}
+            </>
+          )
+        )}
+      </BreadcrumbList>
+    </Breadcrumb>
+
+    // <nav aria-label="Breadcrumb" className="mb-6 block">
+    //   <ol className={clsx(poppins.className, 'flex text-xl md:text-2xl')}>
+    //     {breadcrumbs.map((breadcrumb, index) => (
+    //       <li
+    //         key={breadcrumb.href}
+    //         aria-current={breadcrumb.active}
+    //         className={clsx(
+    //           "mt-2",
+    //           breadcrumb.active ? 'text-gray-900' : 'text-gray-500',
+    //         )}
+    //       >
+    //         <Link href={breadcrumb.href}>{breadcrumb.label}</Link>
+    //         {index < breadcrumbs.length - 1 ? (
+    //           <span className="mx-3 inline-block">/</span>
+    //         ) : null}
+    //       </li>
+    //     ))}
+    //   </ol>
+    // </nav>
   );
 }
